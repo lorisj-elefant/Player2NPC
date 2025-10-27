6ef48c4944ce3f74101cf5bead33912fdfb9e4f0
lorisj-elefant <loris@elefant.gg>
2025-10-27 13:59:10 -0700

    add hint

--- common/src/main/java/com/goodbird/player2npc/Player2NPCClient.java
+++ common/src/main/java/com/goodbird/player2npc/Player2NPCClient.java
@@ -7,16 +7,18 @@ package com.goodbird.player2npc;
 
 import com.goodbird.player2npc.client.gui.CharacterSelectionScreen;
 import com.goodbird.player2npc.client.render.RenderAutomaton;
+import com.goodbird.player2npc.client.util.ClientPersistence;
 import com.goodbird.player2npc.network.AutomatonSpawnPacket;
 import com.mojang.blaze3d.platform.InputConstants.Type;
+import com.player2.playerengine.PlayerEngineClient;
+
+import dev.architectury.event.events.client.ClientPlayerEvent;
 import dev.architectury.event.events.client.ClientTickEvent;
 import dev.architectury.networking.NetworkManager;
 import dev.architectury.registry.client.keymappings.KeyMappingRegistry;
 import dev.architectury.registry.client.level.entity.EntityRendererRegistry;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.network.chat.Component;
-
-import com.player2.playerengine.PlayerEngineClient;
 
 public class Player2NPCClient {
     private static KeyMapping openCharacterScreenKeybind;
@@ -39,6 +41,18 @@ public class Player2NPCClient {
                 "category.player2npc.keys");
         KeyMappingRegistry.register(openCharacterScreenKeybind);
         KeyMappingRegistry.register(ttsEnableKeybind);
+        ClientPlayerEvent.CLIENT_PLAYER_JOIN.register((player) -> {
+            if (!ClientPersistence.getTTStatus()) {
+                player.sendSystemMessage(
+                        Component.literal("Welcome to Player2NPC!"));
+                player.sendSystemMessage(
+                        Component.literal(
+                                "To spawn a companion, press H. (Make sure you have at least one selected characeter at player2.game)"));
+                player.sendSystemMessage(
+                        Component.literal("To toggle text to speech (TTS), press O."));
+                ClientPersistence.saveTTSStatus(true);
+            }
+        });
         ClientTickEvent.CLIENT_POST.register((client) -> {
             if (openCharacterScreenKeybind.consumeClick() && client.level != null) {
                 client.setScreen(new CharacterSelectionScreen());
