a500f0271dc93f70f4f6340a209ff62d6bac3f3e
lorisj-elefant <loris@elefant.gg>
2025-11-05 08:57:56 -0800

    companion spawn fix

--- common/src/main/java/com/goodbird/player2npc/Player2NPC.java
+++ common/src/main/java/com/goodbird/player2npc/Player2NPC.java
@@ -28,9 +28,12 @@ import org.apache.logging.log4j.Logger;
 public class Player2NPC {
     private static final Logger LOGGER = LogManager.getLogger();
     public static final String MOD_ID = "player2npc";
-    public static final ResourceLocation SPAWN_PACKET_ID = ResourceLocation.fromNamespaceAndPath("player2npc", "spawn_automatone");
-    public static final ResourceLocation SPAWN_REQUEST_PACKET_ID = ResourceLocation.fromNamespaceAndPath("player2npc", "request_spawn_automatone");
-    public static final ResourceLocation DESPAWN_REQUEST_PACKET_ID = ResourceLocation.fromNamespaceAndPath("player2npc", "request_despawn_automatone");
+    public static final ResourceLocation SPAWN_PACKET_ID = ResourceLocation.fromNamespaceAndPath("player2npc",
+            "spawn_automatone");
+    public static final ResourceLocation SPAWN_REQUEST_PACKET_ID = ResourceLocation.fromNamespaceAndPath("player2npc",
+            "request_spawn_automatone");
+    public static final ResourceLocation DESPAWN_REQUEST_PACKET_ID = ResourceLocation.fromNamespaceAndPath("player2npc",
+            "request_despawn_automatone");
 
     public Player2NPC() {
     }
@@ -39,22 +42,30 @@ public class Player2NPC {
         return ResourceLocation.fromNamespaceAndPath("player2npc", path);
     }
 
-    public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(MOD_ID, Registries.ENTITY_TYPE);
-    public static RegistrySupplier<EntityType<AutomatoneEntity>> AUTOMATONE = ENTITY_TYPES.register(id("aicompanion"), () -> {
-        LOGGER.info("REGISTER");
-        EntityType<AutomatoneEntity> AUTOMATONE = EntityType.Builder.<AutomatoneEntity>of(AutomatoneEntity::new, MobCategory.CREATURE).sized(EntityType.PLAYER.getWidth(), EntityType.PLAYER.getHeight()).clientTrackingRange(64).updateInterval(1).build(id("aicompanion").toString());
-        LOGGER.info("CREATE");
-        return AUTOMATONE;
-    });
+    public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(MOD_ID,
+            Registries.ENTITY_TYPE);
+    public static RegistrySupplier<EntityType<AutomatoneEntity>> AUTOMATONE = ENTITY_TYPES.register(id("aicompanion"),
+            () -> {
+                LOGGER.info("REGISTER");
+                EntityType<AutomatoneEntity> AUTOMATONE = EntityType.Builder
+                        .<AutomatoneEntity>of(AutomatoneEntity::new, MobCategory.CREATURE)
+                        .sized(EntityType.PLAYER.getWidth(), EntityType.PLAYER.getHeight()).clientTrackingRange(64)
+                        .updateInterval(1).build(id("aicompanion").toString());
+                LOGGER.info("CREATE");
+                return AUTOMATONE;
+            });
 
     public static void onInitialize() {
         LOGGER.info("INIT");
         ENTITY_TYPES.register();
         EntityAttributeRegistry.register(AUTOMATONE, Zombie::createAttributes);
-        NetworkManager.registerReceiver(NetworkManager.Side.C2S, SPAWN_REQUEST_PACKET_ID, AutomatoneSpawnRequestPacket::handle);
-        NetworkManager.registerReceiver(NetworkManager.Side.C2S, DESPAWN_REQUEST_PACKET_ID, AutomatoneDespawnRequestPacket::handle);
+        NetworkManager.registerReceiver(NetworkManager.Side.C2S, SPAWN_REQUEST_PACKET_ID,
+                AutomatoneSpawnRequestPacket::handle);
+        NetworkManager.registerReceiver(NetworkManager.Side.C2S, DESPAWN_REQUEST_PACKET_ID,
+                AutomatoneDespawnRequestPacket::handle);
         PlayerEvent.PLAYER_JOIN.register((player) -> CompanionManager.get(player).summonAllCompanionsAsync());
         PlayerEvent.PLAYER_QUIT.register((player) -> {
+            LOGGER.info("Dismissing for player {} ", player.getName().getString());
             CompanionManager.get(player).dismissAllCompanions();
             CompanionManager.remove(player);
         });
